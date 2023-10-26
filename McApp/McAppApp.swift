a16d88875b488251f2cbca208c47c5cee6bf3032
rafalszu-ives <rafal.szulinski@ives-system.com>
2023-10-26 17:19:12 +0200

    Initial Commit

---
+++ McApp/McAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  McAppApp.swift
+//  McApp
+//
+//  Created by Rafal Szulinski on 26/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct McAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
